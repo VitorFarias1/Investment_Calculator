3bb8bd36966b0fdd149531aa736455371e2256f4
vitorfarias1 <vitor.rodriguesfarias@gmail.com>
2025-10-26 18:11:41 -0300

    wip: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import headerImage from "../assets/investment-calculator-logo.png"
+
+export default function Header() {
+
+    return (
+        <header id="header">
+            <img src={headerImage} alt="invest-calculator-logo" />
+            <h1>Investment Calculator</h1>
+        </header>
+    );
+}
